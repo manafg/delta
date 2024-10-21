604ea89294aa451b8a051b20ae7b598ff6d19cbf
manafG <manafhgh22@gmail.com>
2024-10-22 01:24:18 +0300

    Update GraphPiplineHeader.tsx

--- src/components/pipline/GraphPiplineHeader.tsx
+++ src/components/pipline/GraphPiplineHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CommandBar, ICommandBarItemProps } from '@fluentui/react/lib/CommandBar';
 import { useDnD } from '../../context/Cnavas';
 import { useNodes , useEdges } from '@xyflow/react';
@@ -9,21 +9,58 @@ import { startJob } from '../../api/startJob';
 import { useJobId } from '../../context/GraphContext';
 import { validatePipeline } from '../../api/validatePipline';
 import { publishPipeline } from '../../api/publishPipline';
+import { useAlert } from '../../context/AlertContext';
+import { MessageBarType } from '@fluentui/react';
+import { getPipeline } from '../../api/getPipline';
+import { TextField, IconButton } from '@fluentui/react';
 
 interface PipelineHeaderProps {
   onAddFileReader?: () => void;
   onAddFileWriter?: () => void;
   onAddAggregate?: () => void;
   pipelineId?: string;
+  onUpdatePipelineName?: (newName: string) => void;
 }
 
-const GraphPipelineHeader: React.FC<PipelineHeaderProps> = ({ onAddFileReader , onAddFileWriter, onAddAggregate, pipelineId}) => {
+const GraphPipelineHeader: React.FC<PipelineHeaderProps> = ({ onAddFileReader , onAddFileWriter, onAddAggregate, pipelineId, onUpdatePipelineName }) => {
   const { setGraph , graph , setJobId } = useJobId();
+  const [graphData , setGraphData] = useState<string | null>(null);
   const [_, setType] = useDnD();
-  
+  const { showMessage } = useAlert();
   const nodes = useNodes();
   const edges = useEdges();
+  const [isEditing, setIsEditing] = useState(false);
+  const [pipelineName, setPipelineName] = useState('');
+  const [newPipelineName, setNewPipelineName] = useState('');
 
+  useEffect(() => {
+    const fetchPipeline = async () => {
+      if (pipelineId) {
+        const res = await getPipeline(pipelineId);
+        setGraphData(res.graph);
+        setPipelineName(res.name);
+      }
+    };
+
+    fetchPipeline();
+  }, [pipelineId]);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+    setNewPipelineName(pipelineName);
+  };
+
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+    if (newValue !== undefined) {
+        setNewPipelineName(newValue);
+    }
+  };
+
+  const handleConfirm = () => {
+    setIsEditing(false);
+    setPipelineName(newPipelineName);
+    //onUpdatePipelineName(newPipelineName);
+  };
 
   const onDragStart = (event: React.DragEvent<HTMLDivElement>, nodeType: string ) => {
     if (setType) {
@@ -182,36 +219,75 @@ const GraphPipelineHeader: React.FC<PipelineHeaderProps> = ({ onAddFileReader , 
   ];
 
   const handlePublishPipeline = useCallback(async () => {
-    if (pipelineId) {
-      const res = await publishPipeline(pipelineId);
-      console.log(res);
+    try {
+      if (pipelineId) {
+        const res = await publishPipeline(pipelineId);
+        console.log(res);
+        showMessage("Pipeline published", MessageBarType.success);
+      }
+    } catch (error) {
+      showMessage("Failed to publish should save first", MessageBarType.error);
     }
-  }, [pipelineId]);
+  }, [pipelineId, showMessage]);
 
   const handleValidatePipeline = useCallback(async () => {
-    if (pipelineId) {
-      const res = await validatePipeline(pipelineId);
+    try {
+      if (pipelineId) {
+        const res = await validatePipeline(pipelineId);
       console.log(res);
+      showMessage("Pipeline validated", MessageBarType.success);
+      }
+    } catch (error) {
+      showMessage("Failed to validate pipeline", MessageBarType.error);
     }
-  }, [pipelineId]);
+  }, [pipelineId, showMessage]);
 
   const handleSavePipeline = useCallback(async () => {
    const data = SerlizeSchema(nodes, edges);
     if (pipelineId) {
-      const res = await updatePipelineGraph(pipelineId, data);
-      setGraph(JSON.stringify(data));
+      try {
+        const res = await updatePipelineGraph(pipelineId, data);
+        setGraph(JSON.stringify(data));
+        showMessage("Pipeline saved", MessageBarType.success);
+      } catch (error) {
+        showMessage("Failed to save pipeline", MessageBarType.error);
+      }
     } 
-  }, [nodes, edges , pipelineId , setGraph]);
+  }, [nodes, edges , pipelineId , showMessage , setGraph]);
 
   const handleStartPipeline = useCallback(async () => {
-     if (pipelineId && graph) {
-       const res = await startJob(pipelineId, graph , false);
+    try {
+      if (pipelineId && graph) {
+        const res = await startJob(pipelineId, graph , false);
        setJobId(res.id);
+       showMessage("Pipeline started", MessageBarType.success);
      } 
-   }, [graph , pipelineId , setJobId]);
+    } catch (error) {
+      showMessage("Failed to start pipeline should publish first", MessageBarType.error);
+    }
+   }, [graph , pipelineId, showMessage , setJobId]);
 
   return (
-    <div>
+    <div >
+      {isEditing ? (
+        <TextField
+          style={{ width: '30%'}}
+          value={newPipelineName}
+          onChange={handleInputChange}
+          onBlur={handleConfirm}
+          onKeyDown={(e) => e.key === 'Enter' && handleConfirm()}
+          autoFocus
+        />
+      ) : (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <h3 style={{ margin: 0 }}>{pipelineName}</h3>
+          <IconButton
+            iconProps={{ iconName: 'Edit' }}
+            onClick={handleEditClick}
+            ariaLabel="Edit pipeline name"
+          />
+        </div>
+      )}
       <CommandBar
         items={items}
         ariaLabel="Use left and right arrow keys to navigate between commands"
